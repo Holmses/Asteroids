f6b660af62e36a057877a8b8ba96cf94e99c39ca
zhangdk <zdk@dex-robot.com>
2026-01-16 18:00:55 +0800

    feat commit: add player

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import pygame
-from pygame import display
 
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
+from tmp.Asteroids.player import Player
+
 
 def main():
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
         for event in pygame.event.get():
+            screen.fill((0, 0, 0))
+            player.draw(screen)
             if event.type == pygame.QUIT:
                 return
             pass
-        display.flip()
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
+        # print(f"dt:{dt}")
+
 
 
 if __name__ == "__main__":
